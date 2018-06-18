d72819c1ef674f3f7ec4ef0d3224494ee9681b15
b94752 <adrien.bembaron@outlook.fr>
2018-06-18 17:11:46 +0200

    1806 w new repo

---
+++ src/main/java/fr/epsi/b3/ConcertProjet/domain/ConcertRepository.java
@@ -0,0 +1,7 @@
+package fr.epsi.b3.ConcertProjet.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConcertRepository extends CrudRepository<Client, Long>{
+
+}
---
+++ src/main/java/fr/epsi/b3/ConcertProjet/domain/GroupeRepository.java
@@ -0,0 +1,7 @@
+package fr.epsi.b3.ConcertProjet.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GroupeRepository extends CrudRepository<Client, Long> {
+
+}
